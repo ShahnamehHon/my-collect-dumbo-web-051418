8a1c0429aed4c590b48db01ac1ce2653b6461bbd
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-10 18:20:25 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   empty_array = []
   while i < collection.length
-  empty_array << yield collection[i]
+  empty_array << yield(collection[i])
   i += 1
 end
 collection
